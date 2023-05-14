7701888ef530e1114472e7f9f5ed1c32a4e10131
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-05-14 02:49:39 -0300

    feat: crud de clientes completo

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -68,5 +68,34 @@ export async function getClientesId(req, res){
 
 //alterar cliente por id
 export async function putClientes(req, res){
+    const { name, phone, cpf, birthday } = req.body
+    const {id} = req.params
 
+    try{
+        const cliente = await db.query(`
+        SELECT * FROM customers WHERE id = $1
+        `, [id])
+
+        if(cliente.rows.length===0){
+            return res.status(404).send('Cliente não encontrado.')
+        }
+
+        const clienteComCPF = await db.query(`
+        SELECT * FROM customers WHERE cpf = $1
+        `, [cpf])
+
+        if(clienteComCPF.rows.length>0 && clienteComCPF.rows[0].id !== Number(id)){
+            return res.status(409).send('CPF já cadastrado para outro cliente.')
+        }
+
+        await db.query(
+            'UPDATE customers SET name = $1, phone = $2, birthday = $3 WHERE id = $4',
+            [name, phone, birthday, id]
+        )
+
+        res.status(200).send("Dados alterados com sucesso!")
+        
+    } catch(err){
+        res.status(500).send(err.message)
+    }
 }
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getClientes, getClientesId, postClientes } from "../controllers/clientes.controller.js";
+import { getClientes, getClientesId, postClientes, putClientes } from "../controllers/clientes.controller.js";
 import { validate } from "../middlewares/validateSchema.middleware.js";
 import { clienteSchema } from "../schemas/clientes.schema.js";
 
@@ -8,5 +8,6 @@ const clientesRouter = Router()
 clientesRouter.get('/customers', getClientes)
 clientesRouter.post('/customers', validate(clienteSchema, 400), postClientes)
 clientesRouter.get('/customers/:id', getClientesId)
+clientesRouter.put('/customers/:id', validate(clienteSchema, 400) ,putClientes)
 
 export default clientesRouter
