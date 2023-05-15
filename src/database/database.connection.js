74f12fae7d6a80ad8562963623e4f717de754fca
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-05-15 15:15:12 -0300

    chore: adjust code for deploy on render

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,12 +1,13 @@
 import pg from "pg"
 import dotenv from "dotenv"
-
 dotenv.config()
 
 const { Pool } = pg
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-}
+};
 
-export const db = new Pool(configDatabase)
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
